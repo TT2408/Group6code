895b5c654bce519b7e0ea6db91da15b584bd1b6e
Nicola <825200684@qq.com>
2021-05-20 19:55:33 +0800

    Update synthesis.py

--- synthesis.py
+++ synthesis.py
@@ -10,24 +10,14 @@ from parameters import *
 def synthetize(a0s, f0s, aa, hh, frame_length, sample_rate, device):
     assert a0s.size() == f0s.size()
     assert a0s.size()[1] == aa.size()[1]
-
-    # Initialisation of parameters
     nb_bounds = f0s.size()[1]
     frame_length=30#257 #1024/4
     signal_length = (nb_bounds - 1) * frame_length
     #signal_length=f0s.size()[1]*256
     nb_harms = aa.size()[-1]
 
-    """ Additive part """
-    if INHARMONIC:
-        # Adding inharmonicity factor for piano dataset
-        harm_ranks = torch.arange(nb_harms, device=device) + 1
-        factor = torch.sqrt(1 + INHARMONICITY_FACTOR * harm_ranks.pow(2))
-        harm_ranks = factor * harm_ranks
-    else:
-        harm_ranks = torch.arange(nb_harms, device=device) + 1
 
-    # Multiply f0s by harmonic ranks to get all freqs
+    harm_ranks = torch.arange(nb_harms, device=device) + 1
     ff = f0s.unsqueeze(-1) * harm_ranks.unsqueeze(0).unsqueeze(0)
     max_f = sample_rate / 2.1
     aa[ff >= max_f] = 0.0
@@ -36,37 +26,20 @@ def synthetize(a0s, f0s, aa, hh, frame_length, sample_rate, device):
     f0s = func.interpolate(f0s, size=signal_length, mode="linear",
                            align_corners=True)
     f0s = f0s.squeeze(1)
-
-    # Phase accumulation over time
     phases = 2 * np.pi * f0s / sample_rate
     if MODULAR_PHASE_SUM:
         phases_acc = modular_sum(phases, signal_length)
     else:
         phases_acc = torch.cumsum(phases, dim=1)
-
-    # multiply by harmonic ranks to get phase for all harmonics
     phases_acc = phases_acc.unsqueeze(-1) * harm_ranks
-
-    # Normalize amplitudes with a0
     aa_sum = torch.sum(aa, dim=2)
-    # Avoid 0-div when all amplitudes are 0
     aa_sum[aa_sum == 0.] = 1.
     aa_norm = aa / aa_sum.unsqueeze(-1)
     aa = aa_norm * a0s.unsqueeze(-1)
-
-    # Interpolate amplitudes
     aa = smoothing_amplitudes(aa, signal_length, HAMMING_WINDOW_LENGTH, device)
-
-    # Additive generation
     additive = aa * torch.sin(phases_acc)
-
-    # Sum over harmonics
     additive = torch.sum(additive, dim=2)
-
-    """ Noise part """
     noise = synthetize_noise(hh, device)
-
-    # Empty cache
     torch.cuda.empty_cache()
 
     return additive, noise
@@ -96,7 +69,6 @@ def prevent_aliasing(ff, aa, f_max, f_min):
 
 
 def modular_sum(phases, signal_length):
-    # Computing phase accumulation modulo 2*pi to minimize error
     cursor = 0
     stride = 256
 
